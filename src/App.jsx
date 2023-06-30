b7f96478d5cb5e28b75b15205881dd0281ca8ab5
Dion Charles <dionr_charles@hotmail.com>
2023-06-30 14:48:52 -0400

    change to app.jsx?

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
