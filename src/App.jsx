757e4ea6695dfb08ee05eb074097f52462cf037c
Dion Charles <dionr_charles@hotmail.com>
2023-07-01 12:56:09 -0400

    change two files

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,13 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick() {
-    // some logic here
-  }
-
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={handleClick} key={puppy.id}>
-            {puppy.name}
-          </p>
-        );
-      })}
-    </div>
-  );
-}
-      
+      {puppies.map((puppy) => (
+        <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
       {featPupId && (
         <div>
           <h2>{featuredPup.name}</h2>
@@ -32,4 +22,7 @@ function App() {
           </ul>
         </div>
       )}
+    </div>
+  );
+}
 export default App
