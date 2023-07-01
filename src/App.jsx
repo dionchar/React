afe40351afff5f78711b1ee8f5f451fe4db67313
Dion Charles <dionr_charles@hotmail.com>
2023-07-01 12:22:56 -0400

    changes to two files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
-import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
